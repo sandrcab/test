f074af9d69c8d2a29661a6a3676d16e6e20b456c
sandrcab <37333852+sandrcab@users.noreply.github.com>
2018-03-13 14:02:38 +0100

    Update MeArm.py

--- MeArm.py
+++ MeArm.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
   arm = MeArm()
   #arm.clap()
   #Go up and left to grab something
-  arm.gotoPoint(-.80,1.0,1.4); 
+  arm.gotoPoint(-2,1.8,2); 
   #arm.closeGripper();
   #Go down, forward and right to drop it
   #arm.gotoPoint(.70,2.00,.10);
